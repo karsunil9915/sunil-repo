7672e95fbcca1e754169ce4be9f6c784eee9e522
karsunil9915 <kar.sunil9915@gmail.com>
2018-10-28 10:14:54 +0530

    commiting my project data

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,9 @@
 ''' This is my 1st git commit
       Author : sunil '''
-print ('hello word' )
+print ( 'hello word' )
+
+print ( 'add a new line to file' )
+
+print ( 'also add another line to file' )
 
 
